e1169839277eed40c9bdcf6c5203a7b4cb43a19f
vsnick <vsrinivasanikhil@gmail.com>
2016-08-26 21:29:44 +0530

    crash fix

--- app/src/main/java/vsnick/jasmine/cbit/LoginActivity.java
+++ app/src/main/java/vsnick/jasmine/cbit/LoginActivity.java
@@ -3,12 +3,16 @@ package vsnick.jasmine.cbit;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.DownloadManager;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -43,8 +47,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -71,153 +82,176 @@ public class LoginActivity extends AppCompatActivity{
     }
     public void signIn()
     {
-        final ProgressDialog progress;
-        progress=new ProgressDialog(this);
-        progress.setMessage("Authenticating..");
-        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progress.setIndeterminate(true);
-        progress.setCancelable(false);
-        progress.show();
-        AsyncTask task = new AsyncTask() {
-            Boolean status=false;
-            String username="";
-            String password="";
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                EditText usrname,pass;
-                usrname = (EditText) findViewById(R.id.email);
-                username = usrname.getText().toString();
-                pass = (EditText) findViewById(R.id.password);
-                password = pass.getText().toString();
-            }
-
-            @Override
-            protected Object doInBackground(Object[] params) {
-                Connection.Response conn= null;
-                try {
-                    conn = Jsoup.connect("http://erp.cbit.org.in/Login.aspx")
-                            .ignoreContentType(true)
-                            .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
-                            .followRedirects(true)
-                            .method(Connection.Method.GET)
-                            .execute();
-                } catch (IOException e) {
-                    Log.d("vsn", "doInBackground: ");
-                    e.printStackTrace();
-                }
-                Document login = null;
-                try {
-                    login = conn.parse();
-                } catch (IOException e) {
-                    Log.d("vsn", "doInBackground: ");
-                    e.printStackTrace();
-                }
-                Element ele = login.getElementById("form1");
-                Elements ini = ele.getElementsByTag("input");
-                HashMap<String,String> h = new HashMap<String,String>();
-                for(Element i : ini)
-                {
-                    h.put(i.attr("name"),i.attr("value"));
-                }
-                Connection.Response document= null;
-                try {
-                    document = Jsoup.connect("http://erp.cbit.org.in/Login.aspx")
-                            .data("__LASTFOCUS",h.get("__LASTFOCUS"))
-                            .data("__EVENTTARGET",h.get("__EVENTTARGET"))
-                            .data("__EVENTARGUMENT","__EVENTARGUMENT")
-                            .data("__VIEWSTATE",h.get("__VIEWSTATE"))
-                            .data("__EVENTVALIDATION",h.get("__EVENTVALIDATION"))
-                            .data("txtUserName",username)
-                            .data("btnNext","Next")
-                            .cookies(conn.cookies())
-                            .method(Connection.Method.POST)
-                            .execute();
-                } catch (IOException e) {
-                    Log.d("vsn", "doInBackground: ");
-                    e.printStackTrace();
-                }
-                Document login2 = null;
-                try {
-                    login2 = document.parse();
-                } catch (IOException e) {
-                    Log.d("vsn", "doInBackground: ");
-                    e.printStackTrace();
-                }
-                Element ele2 = login2.getElementById("form1");
-                Elements ini2 = ele2.getElementsByTag("input");
-                HashMap<String,String> g = new HashMap<String,String>();
-                for(Element i : ini2)
-                {
-                    g.put(i.attr("name"),i.attr("value"));
-                }
-                Connection.Response document2= null;
-                try {
-                    document2 = Jsoup.connect("http://erp.cbit.org.in/Login.aspx")
-                            .data("__LASTFOCUS","")
-                            .data("__EVENTTARGET","")
-                            .data("__EVENTARGUMENT","")
-                            .data("__VIEWSTATE",g.get("__VIEWSTATE"))
-                            .data("__EVENTVALIDATION",g.get("__EVENTVALIDATION"))
-                            .data("txtPassword",password)
-                            .data("btnSubmit","Submit")
-                            .cookies(document.cookies())
-                            .method(Connection.Method.POST)
-                            .execute();
-                } catch (IOException e) {
-                    Log.d("vsn", "doInBackground: ");
-                    e.printStackTrace();
-                }
-                //Log.d("vsn", "onPostExecute: "+document2);
-                Document loginStat = null;
-                try {
-                    loginStat = document2.parse();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Element title= loginStat.getElementsByTag("title").first();
-                if(title.text().equals("Student Login"))
-                {
-                    status =true;
-                    FileOutputStream outputStream;
-                    try {
-                        outputStream = openFileOutput("Attendance", Context.MODE_PRIVATE);
-                        outputStream.write(loginStat.toString().getBytes());
-                        outputStream.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    String imgUrl = loginStat.getElementById("ctl00_cpHeader_ucStud_ImgStudPic").attr("src");
-                    Log.d("vsn", "doInBackground:"+imgUrl);
-                }
-                return null;
-            }
-            @Override
-            protected void onPostExecute(Object o) {
-                super.onPostExecute(o);
-                progress.dismiss();
-                if(status)
-                {
-                    SharedPreferences sharedPref = getSharedPreferences("myfile",Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = sharedPref.edit();
-                    editor.putBoolean("first",false);
-                    editor.putString("username",username);
-                    editor.putString("password",password);
-                    editor.commit();
-                    finish();
-                }
-                else
-                {
-                    TextView error = (TextView) findViewById(R.id.error);
-                    error.setVisibility(View.VISIBLE);
-                }
-            }
-        };
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
+            final ProgressDialog progress;
+            progress=new ProgressDialog(this);
+            progress.setMessage("Authenticating..");
+            progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progress.setIndeterminate(true);
+            progress.setCancelable(false);
+            progress.show();
+            AsyncTask task = new AsyncTask() {
+                Boolean status=false,error=false;
+                String username="";
+                String password="";
+                String name_of_user="";
+                String year="";
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    EditText usrname,pass;
+                    usrname = (EditText) findViewById(R.id.email);
+                    username = usrname.getText().toString();
+                    pass = (EditText) findViewById(R.id.password);
+                    password = pass.getText().toString();
+                }
+
+                @Override
+                protected void onProgressUpdate(Object[] values) {
+                    super.onProgressUpdate(values);
+                    progress.setMessage(values[0].toString());
+                }
+
+                @Override
+                protected Object doInBackground(Object[] params) {
+                    try {
+                        Connection.Response conn = null;
+                        conn = Jsoup.connect("http://erp.cbit.org.in/Login.aspx")
+                                .ignoreContentType(true)
+                                .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                                .followRedirects(true)
+                                .method(Connection.Method.GET)
+                                .execute();
+                        Log.d("vsn", "doInBackground: " + conn);
+                        Document login = null;
+                        login = conn.parse();
+                        Element ele = login.getElementById("form1");
+                        Elements ini = ele.getElementsByTag("input");
+                        HashMap<String, String> h = new HashMap<String, String>();
+                        for (Element i : ini) {
+                            h.put(i.attr("name"), i.attr("value"));
+                        }
+                        Connection.Response document = null;
+                        document = Jsoup.connect("http://erp.cbit.org.in/Login.aspx")
+                                .data("__LASTFOCUS", h.get("__LASTFOCUS"))
+                                .data("__EVENTTARGET", h.get("__EVENTTARGET"))
+                                .data("__EVENTARGUMENT", "__EVENTARGUMENT")
+                                .data("__VIEWSTATE", h.get("__VIEWSTATE"))
+                                .data("__EVENTVALIDATION", h.get("__EVENTVALIDATION"))
+                                .data("txtUserName", username)
+                                .data("btnNext", "Next")
+                                .cookies(conn.cookies())
+                                .method(Connection.Method.POST)
+                                .execute();
+                        String imgUrl = null;
+                        imgUrl = document.parse().getElementById("ImgUserPhoto").attr("src");
+                        Document login2 = null;
+                        login2 = document.parse();
+                        Element ele2 = login2.getElementById("form1");
+                        Elements ini2 = ele2.getElementsByTag("input");
+                        HashMap<String, String> g = new HashMap<String, String>();
+                        for (Element i : ini2) {
+                            g.put(i.attr("name"), i.attr("value"));
+                        }
+                        Connection.Response document2 = null;
+                        document2 = Jsoup.connect("http://erp.cbit.org.in/Login.aspx")
+                                .data("__LASTFOCUS", "")
+                                .data("__EVENTTARGET", "")
+                                .data("__EVENTARGUMENT", "")
+                                .data("__VIEWSTATE", g.get("__VIEWSTATE"))
+                                .data("__EVENTVALIDATION", g.get("__EVENTVALIDATION"))
+                                .data("txtPassword", password)
+                                .data("btnSubmit", "Submit")
+                                .cookies(document.cookies())
+                                .method(Connection.Method.POST)
+                                .execute();
+                        //Log.d("vsn", "onPostExecute: "+document2);
+                        Document loginStat = null;
+                        loginStat = document2.parse();
+                        Element title = loginStat.getElementsByTag("title").first();
+                        if (title.text().equals("Student Login")) {
+                            publishProgress("Grabbing Attendance");
+                            status = true;
+                            FileOutputStream outputStream;
+                            outputStream = openFileOutput("Attendance", Context.MODE_PRIVATE);
+                            outputStream.write(loginStat.toString().getBytes());
+                            outputStream.close();
+                            Bitmap bitmap;
+                            URL url = new URL("http://erp.cbit.org.in/" + imgUrl);
+                            URLConnection connec = url.openConnection();
+                            bitmap = BitmapFactory.decodeStream(connec.getInputStream());
+                            File file = new File(getFilesDir(), "usrPic.jpg");
+                            FileOutputStream out = new FileOutputStream(file);
+                            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                            out.flush();
+                            out.close();
+                            String s = loginStat.getElementById("ctl00_cpHeader_ucStud_lblStudentStatus").text();
+                            name_of_user = s.substring(8, s.indexOf("(") - 1);
+                            year = s.substring(s.lastIndexOf(',') + 2);
+                            publishProgress("Downloading Syllabus..");
+                            //Downloading Syllabus
+                            //progress.setMessage("Downloading Syllabus..");
+                            String pdfUrl = "http://cbit.ac.in/files/BE%20-%20CSE%20-%20III%20Year%20-%20Scheme%20and%20Syllabus.pdf";
+                            url = new URL(pdfUrl);
+                            URLConnection conection = url.openConnection();
+                            conection.connect();
+                            // download the file
+                            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                            // Output stream
+                            OutputStream output = new FileOutputStream(Environment.getExternalStorageDirectory().toString() + "/internal/Sysllabus.pdf");
+                            byte data[] = new byte[1024];
+
+                            int count = 0;
+
+                            while ((count = input.read(data)) != -1) {
+                                // writing data to file
+                                output.write(data, 0, count);
+                            }
+                            output.flush();
+                            output.close();
+                            input.close();
+                        }
+
+                    }catch(SocketTimeoutException e){
+                        e.printStackTrace();
+                    }
+                    catch(Exception e) {
+                        e.printStackTrace();
+                        error = true;
+                    }
+                    return null;
+                }
+                @Override
+                protected void onPostExecute(Object o) {
+                    super.onPostExecute(o);
+                    progress.dismiss();
+                    if(!error)
+                    {
+                        Toast.makeText(getApplicationContext(),"Server error",Toast.LENGTH_SHORT).show();
+                    } else if(status)
+                    {
+                        SharedPreferences sharedPref = getSharedPreferences("myfile",Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putBoolean("first",false);
+                        editor.putString("username",username);
+                        editor.putString("password",password);
+                        editor.putString("name_of_user",name_of_user);
+                        editor.putString("year",year);
+                        editor.commit();
+                        finish();
+                    }
+                    else
+                    {
+                        TextView error = (TextView) findViewById(R.id.error);
+                        error.setVisibility(View.VISIBLE);
+                    }
+                }
+            };
             task.execute();
         } else {
             Toast.makeText(getApplicationContext(),"Internet required",Toast.LENGTH_SHORT).show();
--- app/src/main/java/vsnick/jasmine/cbit/MainActivity.java
+++ app/src/main/java/vsnick/jasmine/cbit/MainActivity.java
@@ -1,11 +1,20 @@
 package vsnick.jasmine.cbit;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,16 +24,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private Bundle bundle;
+    private TextView hallTicNO,name;
+    SharedPreferences sharedPreferences;
+    ImageView imageViewRound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        bundle=savedInstanceState;
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -35,12 +57,29 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        SharedPreferences sharedPreferences =getSharedPreferences("myfile", Context.MODE_PRIVATE);
+        sharedPreferences =getSharedPreferences("myfile", Context.MODE_PRIVATE);
         Boolean firstTime = sharedPreferences.getBoolean("first",true);
-        if(true)
+        if(firstTime)
         {
             startActivity(new Intent(this,LoginActivity.class));
+            if(firstTime)
+                finish();
         }
+        View hView =  navigationView.getHeaderView(0);
+        imageViewRound=(ImageView)hView.findViewById(R.id.imageView_round);
+        name = (TextView) hView.findViewById(R.id.name);
+        hallTicNO = (TextView) hView.findViewById(R.id.hallticno);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        name.setText(sharedPreferences.getString("name_of_user",""));
+        hallTicNO.setText(sharedPreferences.getString("username",""));
+        File file = new File(getFilesDir(),"usrPic.jpg");
+        Bitmap b = BitmapFactory.decodeFile(file.getAbsolutePath());
+        Log.d("vsn", "onCreate: "+b);
+        imageViewRound.setImageBitmap(b);
     }
 
     @Override
@@ -68,7 +107,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.change_user) {
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            onResume();
             return true;
         }
 
@@ -84,6 +125,14 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.attendance) {
             Intent intent = new Intent(getApplicationContext(),Attendance.class);
             startActivity(intent);
+        }else if(id == R.id.syllabus){
+            getExternalFilesDir("pdf");
+            File path = new File(getFilesDir(),"internal/");
+            File pdffile = new File(path,"Syllabus.pdf");
+            Intent shareIntent = new Intent(Intent.ACTION_VIEW,Uri.parse(pdffile.toString()));
+            shareIntent.setDataAndType(Uri.parse(pdffile.toString()),"application/pdf");
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(shareIntent);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
